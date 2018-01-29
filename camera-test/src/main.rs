64382c5767160af8259dc6849551d919ca41cf1a
Tim <timothyhollabaugh@gmail.com>
2018-01-29 17:25:30 -0500

    Destroy components and create from name enum

--- camera-test/src/main.rs
+++ camera-test/src/main.rs
@@ -1,13 +1,18 @@
 extern crate rpi_mmal;
 
 use rpi_mmal::component::Component;
+use rpi_mmal::component::ComponentName;
 //use rpi_mmal_sys::vc_gencmd_number_property;
 
 fn main() {
     println!("Hello, world!");
 
-    let component = Component::new(b"vc.camera_info\x00");
-    //let component = Component::new(b"none\x00");
+    //let component = Component::new(b"vc.camera_info\x00");
+    let component = Component::new(ComponentName::CameraInfo);
     println!("Component: {:?}", component);
+
+    if let Ok(component) = component {
+        component.destroy();
+    }
 
 }
--- rpi-mmal/src/component.rs
+++ rpi-mmal/src/component.rs
@@ -3,40 +3,71 @@ use std::mem;
 use std::ffi::CStr;
 use rpi_mmal_sys::*;
 
-//enum ComponentDefault {
-//    AudioDecoder = MMAL_COMPONENT_DEFAULT_AUDIO_DECODER,
-//    AudioRender = MMAL_COMPONENT_DEFAULT_AUDIO_RENDERER,
-//    Camera = MMAL_COMPONENT_DEFAULT_CAMERA,
-//    CameraInfo = MMAL_COMPONENT_DEFAULT_CAMERA_INFO,
-//    Clock = MMAL_COMPONENT_DEFAULT_CLOCK,
-//    ContainerReader = MMAL_COMPONENT_DEFAULT_CONTAINER_READER,
-//    ContainerWriter = MMAL_COMPONENT_DEFAULT_CONTAINER_WRITER,
-//    ImageDecoder = MMAL_COMPONENT_DEFAULT_IMAGE_DECODER,
-//    ImageEncoder = MMAL_COMPONENT_DEFAULT_IMAGE_ENCODER,
-//    Miracast = MMAL_COMPONENT_DEFAULT_MIRACAST,
-//    Scheduler = MMAL_COMPONENT_DEFAULT_SCHEDULER,
-//    Splitter = MMAL_COMPONENT_DEFAULT_SPLITTER,
-//    VideoConverter = MMAL_COMPONENT_DEFAULT_VIDEO_CONVERTER,
-//    VideoDecoder = MMAL_COMPONENT_DEFAULT_VIDEO_DECODER,
-//    VideoEncoder = MMAL_COMPONENT_DEFAULT_VIDEO_ENCODER,
-//    VideoInjecter = MMAL_COMPONENT_DEFAULT_VIDEO_INJECTER,
-//    VideoRenderer = MMAL_COMPONENT_DEFAULT_VIDEO_RENDERER,
-//    VideoSplitter = MMAL_COMPONENT_DEFAULT_VIDEO_SPLITTER,
-//}
+/// The names of default MMAL components
+pub enum ComponentName {
+    AudioDecoder,
+    AudioRender,
+    Camera,
+    CameraInfo,
+    Clock,
+    ContainerReader,
+    ContainerWriter,
+    ImageDecoder,
+    ImageEncoder,
+    Miracast,
+    Scheduler,
+    Splitter,
+    VideoConverter,
+    VideoDecoder,
+    VideoEncoder,
+    VideoInjecter,
+    VideoRenderer,
+    VideoSplitter,
+    Other(&'static [u8]),
+}
 
+impl Into<&'static [u8]> for ComponentName {
+    fn into(self) -> &'static [u8] {
+        match self {
+            ComponentName::AudioDecoder => MMAL_COMPONENT_DEFAULT_AUDIO_DECODER,
+            ComponentName::AudioRender => MMAL_COMPONENT_DEFAULT_AUDIO_RENDERER,
+            ComponentName::Camera => MMAL_COMPONENT_DEFAULT_CAMERA,
+            ComponentName::CameraInfo => MMAL_COMPONENT_DEFAULT_CAMERA_INFO,
+            ComponentName::Clock => MMAL_COMPONENT_DEFAULT_CLOCK,
+            ComponentName::ContainerReader => MMAL_COMPONENT_DEFAULT_CONTAINER_READER,
+            ComponentName::ContainerWriter => MMAL_COMPONENT_DEFAULT_CONTAINER_WRITER,
+            ComponentName::ImageDecoder => MMAL_COMPONENT_DEFAULT_IMAGE_DECODER,
+            ComponentName::ImageEncoder => MMAL_COMPONENT_DEFAULT_IMAGE_ENCODER,
+            ComponentName::Miracast => MMAL_COMPONENT_DEFAULT_MIRACAST,
+            ComponentName::Scheduler => MMAL_COMPONENT_DEFAULT_SCHEDULER,
+            ComponentName::Splitter => MMAL_COMPONENT_DEFAULT_SPLITTER,
+            ComponentName::VideoConverter => MMAL_COMPONENT_DEFAULT_VIDEO_CONVERTER,
+            ComponentName::VideoDecoder => MMAL_COMPONENT_DEFAULT_VIDEO_DECODER,
+            ComponentName::VideoEncoder => MMAL_COMPONENT_DEFAULT_VIDEO_ENCODER,
+            ComponentName::VideoInjecter => MMAL_COMPONENT_DEFAULT_VIDEO_INJECTER,
+            ComponentName::VideoRenderer => MMAL_COMPONENT_DEFAULT_VIDEO_RENDERER,
+            ComponentName::VideoSplitter => MMAL_COMPONENT_DEFAULT_VIDEO_SPLITTER,
+            ComponentName::Other(name) => name,
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct Component(Box<MMAL_COMPONENT_T>);
 
 impl Component {
-    pub fn new(name: &'static [u8]) -> Result<Component, MMAL_STATUS_T> {
+
+    /// Create a new component with a ComponentName
+    /// The component MUST be destroyed with Component::destroy()
+    /// to avoid leaking memory
+    pub fn new(name: ComponentName) -> Result<Component, MMAL_STATUS_T> {
 
         let mmal_component: Box<MMAL_COMPONENT_T> = unsafe { Box::new(mem::uninitialized()) };
 
         // FIXME
         // Handle unwrap correctly
         // Should really find better way to get name
-        let name_cstr = CStr::from_bytes_with_nul(name).unwrap();
+        let name_cstr = CStr::from_bytes_with_nul(name.into()).unwrap();
 
         let (status, mmal_component) = unsafe {
             let mut mmal_component_pointer = Box::into_raw(mmal_component);
@@ -51,24 +82,27 @@ impl Component {
             (status, mmal_component)
         };
 
-        println!("status: {:?}", status);
+        //println!("status: {:?}", status);
 
         if status == MMAL_STATUS_T::MMAL_SUCCESS {
-            println!("mmal_component: {:?}", mmal_component);
+            //println!("mmal_component: {:?}", mmal_component);
             Ok(Component(mmal_component))
         } else {
             Err(status)
         }
     }
-}
 
-impl Drop for Component {
-    fn drop(&mut self) {
-
-        println!("Destroying component: {:?}", self.0);
+    /// Destroy the component by calling mmal_component_destroy
+    /// This must be called before the program ends to avoid
+    /// leaking memory
+    ///
+    /// This really should be in a drop trait, but the drop trait
+    /// takes an &mut self, and Box::into_raw takes ownership.
+    pub fn destroy(self) {
+        //println!("Destroying component: {:?}", &self.0);
 
         let status = unsafe {
-            let mut mmal_component_pointer = Box::into_raw(&self.0);
+            let mut mmal_component_pointer = Box::into_raw(self.0);
             mmal_component_destroy(mmal_component_pointer)
         };
 
@@ -77,3 +111,4 @@ impl Drop for Component {
         }
     }
 }
+
